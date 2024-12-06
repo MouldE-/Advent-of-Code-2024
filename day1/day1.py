bef2b2e2ae4214b5312b0345fa366f5307eca209
Tim Revans <timothy.revans@hotmail.co.uk>
2024-12-06 19:36:59 +0000

    Day 1 Solution Part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,13 @@
+# part 1
 with open("day1-input", "r") as file:
     nums = [int(num) for num in file.read().split()]
 l1, l2 = nums[0:len(nums):2], nums[1:len(nums):2]
 l1.sort()
 l2.sort()
 print(sum([abs(item1 - item2) for item1, item2 in zip(l1, l2)]))
+
+# part 2
+d2 = {}
+for item in l2:
+    d2[item] = d2.get(item, 0) + 1
+print(sum([d2.get(item, 0) * item for item in l1]))
