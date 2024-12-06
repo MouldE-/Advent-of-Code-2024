f1679ec7623cc0454fbb6bdedfefde82cb6991e4
Tim Revans <timothy.revans@hotmail.co.uk>
2024-12-06 21:51:23 +0000

    Day 3 Solution Part 2

--- day3/day3.py
+++ day3/day3.py
@@ -2,3 +2,19 @@
 import re
 with open("day3-input", "r") as file:
     print(sum(int(hit[0])*int(hit[1]) for hit in re.findall("mul\(([0-9]*),([0-9]*)\)", file.read())))
+
+# part 2
+count = 0
+with open("day3-input", "r") as file:
+    enabled = True
+    for hit in re.findall("mul\([0-9]*,[0-9]*\)|don't\(\)|do\(\)", file.read()):
+        hit = hit.strip("/")
+        if hit == "do()":
+            enabled = True
+        elif hit == "don't()":
+            enabled = False
+        else:
+            match = re.findall("mul\(([0-9]*),([0-9]*)\)", hit)[0]
+            if enabled:
+                count += int(match[0]) * int(match[1])
+print(count)
