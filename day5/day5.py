6d793fd08d90386c80ebb1f8b45d83579e9b96d4
Tim Revans <timothy.revans@hotmail.co.uk>
2024-12-07 13:06:18 +0000

    Day 5 Solution Part 2

--- day5/day5.py
+++ day5/day5.py
@@ -2,7 +2,7 @@
 rules:dict[str, str] = {}
 manuals:list[str] = []
 
-def isValid(pages:list[str]):
+def isValid(pages:list[str]) -> bool:
     for i in range(len(pages)):
         if rules.get(pages[i]) == None:
             continue
@@ -11,11 +11,22 @@ def isValid(pages:list[str]):
                 return False
     return True
 
+def reorder(pages:list[str]):
+    for i in range(len(pages)):
+        if rules.get(pages[i]) == None:
+            continue
+        for j in range(i):
+            if pages[j] in rules[pages[i]]:
+                pages.insert(j, pages.pop(i))
+
 with open("day5-input", "r") as file:
     while (line := file.readline()) != "\n":
         key, value = line[:-1].split("|")
         rules[key] = rules.get(key, []) + [value]
     manuals = file.readlines()
 
-count = sum([int(pages[len(pages)//2]) for pages in [manual.strip("\n").split(",") for manual in manuals] if isValid(pages)])
-print(count)
+print(sum([int(pages[len(pages)//2]) for pages in [manual.strip("\n").split(",") for manual in manuals] if isValid(pages)]))
+
+# part 2
+print(sum([int(pages[len(pages)//2]) for pages in [manual.strip("\n").split(",") for manual in manuals] if not isValid(pages) and reorder(pages) == None]))
+
